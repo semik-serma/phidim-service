60467455b28cd15a61e32fbb932ffdfb62bf56a3
semik-serma <semikserma@gmail.com>
2026-08-17 10:20:19 +0545

    fix(prod): resolve TDZ ReferenceError in UI button and point websockets to production backend

--- index.js
+++ index.js
@@ -13,17 +13,18 @@ export { AuthProvider, useAuth } from "./context/AuthContext.js";
 export { getSocket, disconnectSocket, subscribeToEvent } from "./realtime/client.js";
 export { default as VideoCall } from "./realtime/VideoCall.jsx";
 
-// Re-export components directly from src/components
-export { default as Navbar } from "../src/components/Navbar.js";
-export { default as Footer } from "../src/components/Footer.js";
-export { default as Header } from "../src/components/Header.js";
-export { default as TopBar } from "../src/components/TopBar.js";
-export { default as HeroCarousel } from "../src/components/HeroCarousel.js";
-export { default as WeatherWidget } from "../src/components/WeatherWidget.js";
-export { default as WhatsAppWidget } from "../src/components/WhatsAppWidget.js";
-export { default as AuthModal } from "../src/components/AuthModal.js";
-export { default as AboutModal } from "../src/components/AboutModal.js";
-export { default as ContactModal } from "../src/components/ContactModal.js";
-export { default as ServiceBookingModal } from "../src/components/ServiceBookingModal.js";
-export { default as LogoutConfirmModal } from "../src/components/LogoutConfirmModal.js";
-export { default as RoleGuard } from "../src/components/RoleGuard.js";
+// Re-export components directly from ./components
+export { Navbar } from "./components/Navbar.js";
+export { Footer } from "./components/Footer.js";
+export { Header } from "./components/Header.js";
+export { TopBar } from "./components/TopBar.js";
+export { HeroCarousel } from "./components/HeroCarousel.js";
+export { WeatherWidget } from "./components/WeatherWidget.js";
+export { WhatsAppWidget } from "./components/WhatsAppWidget.js";
+export { AuthModal } from "./components/AuthModal.js";
+export { AboutModal } from "./components/AboutModal.js";
+export { ContactModal } from "./components/ContactModal.js";
+export { ServiceBookingModal } from "./components/ServiceBookingModal.js";
+export { LogoutConfirmModal } from "./components/LogoutConfirmModal.js";
+export { RoleGuard } from "./components/RoleGuard.js";
+
